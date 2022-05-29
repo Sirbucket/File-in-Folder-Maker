f4209d6813511834302aa8018a3aa3cf4c889b28
Sirbucket <51178809+Sirbucket@users.noreply.github.com>
2022-05-29 01:55:03 -0400

    close out by typing close rather than quit()

--- main.py
+++ main.py
@@ -34,10 +34,8 @@ def write_file_in_dir():
     if not os.path.exists(parent_dir):
         print("That is not a path that currently exists your pc, please try again!"); 
         parent_dir = input("Please state the parent directory."); 
-    child_dir = input("Please state the child directory."); 
-    dir = os.path.join(parent_dir,child_dir); 
     file_name = input("Please state the name of the file with the extension you desire! (Only accepts text formats.)"); 
-    path = os.path.join(dir, file_name); 
+    path = os.path.join(parent_dir, file_name); 
     write_file(path); 
     if os.path.exists(path):
         print("The file was created successfully!"); 
@@ -49,9 +47,11 @@ def write_file_in_dir():
 
 
 def program():
-    user_input = input("Please define what you would like to do. \nType write file to create a file in your current directory. \nType read file to read a file in your current directory. \nType remove file to remove a file in your current directory. \nType create dir to create a directory in your current directory. \nType remove dir to remove a directory in your current directory. \nType write file in dir to write a file into a directory anywhere on your pc. \nType remove file in dir to remove a file from a directory anywhere on your pc. \nType remove dir from dir to remove a directory from anywhere on your PC. \nType create dir in dir to create a directory from anywhere on your PC. \n").lower(); 
+    user_input = input("Please define what you would like to do. \nType write file to create a file in your current directory. \nType read file to read a file in your current directory. \nType remove file to remove a file in your current directory. \nType create dir to create a directory in your current directory. \nType remove dir to remove a directory in your current directory. \nType write file in dir to write a file into a directory anywhere on your pc. \nType remove file in dir to remove a file from a directory anywhere on your pc. \nType remove dir from dir to remove a directory from anywhere on your PC. \nType create dir in dir to create a directory from anywhere on your PC. \nType close to end the program.").lower(); 
     
     if user_input == "write file in dir":
         write_file_in_dir(); 
+    if user_input == "close":
+        return; 
 
 program(); 
